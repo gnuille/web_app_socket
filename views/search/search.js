bf0ae90345d38f93cb8a6fe3d608402ba02d952e
teleportex <guillem.ramirez.miranda@est.fib.upc.edu>
2017-11-15 22:50:34 +0100

    begin join room protocol

--- views/search/search.js
+++ views/search/search.js
@@ -5,14 +5,18 @@ function writeTables(res){
   var html = '<tr>';
   for (var i = 0; i < res.length; ++i) {
       var name = res[i].name;
-      html += '<td>'+name+'</td>' + '<td><button id='+name+' onClick="joinLob(this.id)">Join!</button></td> </tr>' + '<tr>' ;
+      html += '<td>'+name+'</td>' + '<td><button id=\''+name+'\' onClick="joinLob(this.id)">Join!</button></td> </tr>' + '<tr>' ;
   };
   $('#list').append(html);
 }
 
 function search(){
-  console.log(document.getElementById("key").value)
   socket.emit("gather_tables", document.getElementById("key").value)
+}
+
+function joinLob(tablename){
+  //read nick cookie
+  socket.emit("join_lobby", tablename)
 }
 
 socket.emit("gather_tables", "")
